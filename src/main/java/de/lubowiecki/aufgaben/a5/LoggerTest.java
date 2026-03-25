50a445726df654d7c865152c9a8f79283665bfbc
tlubowiecki <tomasz@lubowiecki.de>
2026-03-25 16:28:47 +0100

    Aufgabe 5

---
+++ src/main/java/de/lubowiecki/aufgaben/a5/LoggerTest.java
@@ -0,0 +1,23 @@
+package de.lubowiecki.aufgaben.a5;
+
+public class LoggerTest {
+
+    /*
+    Implementiere eine Logging-Klasse. Die Klasse soll als UtilityKlasse (nur statische Methoden enthalten)
+    genutzt werden und beinhaltet einen String, der die Logeinträge sammelt.
+
+    Über den Aufruf der Methode log(…) werden die Log-Einträge erfasst.
+
+    Jeder Log-Eintrag bekommt ein Datum und Uhrzeit, Typ, Klassenname/Methodenname und eine Nachricht z.B.
+
+    15.07.2025 14:55: DEBUG - DebugTest/main - Inhalt von x = 10
+
+    Jeder Eintrag soll eine eigene Zeile produzieren.
+
+    Es soll möglich sein, den Inhalt des Loggers als String, der alles enthält abzufragen.
+    Schreibe eine Testklasse, so du das Logging in der Main-Methode
+    und zwei weiteren Methoden testest.
+     */
+
+
+}
