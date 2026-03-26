85e6e50f5c9719887475265ec60e778327fbfb7c
tlubowiecki <tomasz@lubowiecki.de>
2026-03-26 15:24:12 +0100

    Aufgabe 7

--- src/main/java/de/lubowiecki/aufgaben/a7/Aufgabe.java
+++ src/main/java/de/lubowiecki/aufgaben/a7/Aufgabe.java
@@ -2,6 +2,7 @@ package de.lubowiecki.aufgaben.a7;
 
 public class Aufgabe {
 
+    // Erweiterung zu a2
     // TODO: Erweitert den Würfelbecher so, dass er eine zusätzliche roll-Methode hat
     // TODO: bei Aufruf wird die Anzahl gewünschter Ergebnisse übergeben und als Arrays von Zufallszahlen zurückgegeben
     // TODO: z.B. int[] roll(10);
