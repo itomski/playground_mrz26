49b31788fafae4df78438e0dc5ca552574af4aaa
tlubowiecki <tomasz@lubowiecki.de>
2026-03-31 09:47:38 +0200

    Aufgabe 9, Lösungsansatz

---
+++ src/main/java/de/lubowiecki/aufgaben/a9/Produkt.java
@@ -0,0 +1,40 @@
+package de.lubowiecki.aufgaben.a9;
+
+import java.time.LocalDate;
+
+public class Produkt {
+
+    private String name;
+    private String beschreibung;
+    private int menge;
+    private double price;
+    private LocalDate imBestandSeit;
+
+    public Produkt(String name, String beschreibung, int menge, double price) {
+        this.name = name;
+        this.beschreibung = beschreibung;
+        this.menge = menge;
+        this.price = price;
+        imBestandSeit = LocalDate.now();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getBeschreibung() {
+        return beschreibung;
+    }
+
+    public int getMenge() {
+        return menge;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public LocalDate getImBestandSeit() {
+        return imBestandSeit;
+    }
+}
--- src/main/java/de/lubowiecki/aufgaben/a9/Produktverwaltung.java
+++ src/main/java/de/lubowiecki/aufgaben/a9/Produktverwaltung.java
@@ -3,7 +3,7 @@ package de.lubowiecki.aufgaben.a9;
 public class Produktverwaltung {
 
     /*
-    Übung 1
+    Übung
     Schreibe ein Kommandozeilen-Programm welches eine Sammlung von Produkten verwalten
     kann. Jedes Produkt soll ein Objekt sein, welches Datum (Aufnahme in den Bestand), Name,
     Beschreibung, Menge und Preis als Eigenschaften enthält.
@@ -16,4 +16,37 @@ public class Produktverwaltung {
     Programms gehen alle eingegeben Daten verloren.
     */
 
+    private static final String TAB_ROW = "| %-15s | %-20s | %6d | %6.2f € | \n";
+
+    public static void main(String[] args) {
+
+        int pos = 0;
+
+        Produkt[] produkte = new Produkt[10];
+        produkte[pos++] = new Produkt("Butter", "Lecker", 100, 1.99);
+        produkte[pos++] = new Produkt("Milch", "1,5% Fett", 100, 1.19);
+        produkte[pos++] = new Produkt("Milch", "3,5% Fett", 100, 1.29);
+
+        /*
+        0: Produkt
+        1: Produkt
+        2: Produkt
+        3: null
+        4: null
+        5: null
+        6: null
+        7: null
+        8: null
+        9: null
+        */
+
+        printProduct(produkte);
+    }
+
+    private static void printProduct(Produkt[] produkte) {
+        for(Produkt p : produkte) {
+            if(p == null) break;
+            System.out.printf(TAB_ROW, p.getName(), p.getBeschreibung(), p.getMenge(), p.getPrice());
+        }
+    }
 }
