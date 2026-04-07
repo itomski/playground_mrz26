02f36d185eb49346d8826aa6f97930eaa33707b4
tlubowiecki <tomasz@lubowiecki.de>
2026-04-07 13:37:40 +0200

    Vererbung

--- src/main/java/de/lubowiecki/aufgaben/a13/InterfaceTest.java
+++ src/main/java/de/lubowiecki/aufgaben/a13/InterfaceTest.java
@@ -5,7 +5,9 @@ public class InterfaceTest {
     /*
     Schreibe zwei Klassen für ein Gehege in dem unterschiedliche Tiere hinzugefügft werden können.
     1x Fleischfresser und 1x Vegetarier. Realisiert dies als Interface.
+    Das Inteface hat eine Methode fressen.
     Schreibe danach 6 Tierklassen von denen 3 Fleischfresser und 3 Vegetarier sind.
+    Und 3 die beides sind.
     Teste die Klassen.
      */
 
